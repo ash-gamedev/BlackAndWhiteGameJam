1b6803a11f3f5fe318f68ea879ca01cd8b3a11b9
ash-gamedev <adrouill1997@gmail.com>
2022-04-02 16:46:16 -0400

    Paths working

--- BlackAndWhite_GameJam/Assets/Scripts/GridController.cs
+++ BlackAndWhite_GameJam/Assets/Scripts/GridController.cs
@@ -22,6 +22,14 @@ public class GridController : MonoBehaviour
 
     TileManager tileManager;
 
+    //
+    bool mousePressedDown = false;
+    bool isDrawingPath = false;
+    Tile startOriginalTile = null;
+    Vector3Int startTilePosition;
+    Vector3Int previousTilePosition;
+    Tile lastTile = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -46,7 +54,33 @@ public class GridController : MonoBehaviour
         // Left mouse click -> add path tile (if within bounds)
         if (Input.GetMouseButton(0) && interactiveMap.cellBounds.Contains(mousePos))
         {
+            // if starting to draw a path, save the starting path details and set bool
+            if (isDrawingPath == false)
+            {
+                ResetTilesToDefault();
+
+                isDrawingPath = true;
+                Vector3Int? closestNeighbourPos = GetStartingNeighbour(mousePos);
+                if (closestNeighbourPos != null)
+                {
+                    
+                    startTilePosition = (Vector3Int)closestNeighbourPos;
+                    startOriginalTile = pathMap.GetTile<Tile>((Vector3Int)closestNeighbourPos);
+                    previousTilePosition = startTilePosition;
+                    
+                }
+                else
+                {
+                    previousTilePosition = mousePos;
+                    lastTile = upTile;
+                }
+            }
+
             SetTiles(mousePos);
+        }
+        else
+        {
+            isDrawingPath = false;
         }
 
         // Right mouse click -> remove path tile (if within bounds)
@@ -62,46 +96,61 @@ public class GridController : MonoBehaviour
         return grid.WorldToCell(mouseWorldPos);
     }
 
-    private void SetTiles(Vector3Int gridPosition)
+    Vector3Int? GetStartingNeighbour(Vector3Int gridPosition)
     {
         Dictionary<EnumNeighbour, Tuple<EnumTileDirection, Vector3Int>> neighbourTiles = tileManager.GetAllNeighborTiles(gridPosition);
-        
-        foreach(KeyValuePair<EnumNeighbour, Tuple<EnumTileDirection, Vector3Int>> neighbourTile in neighbourTiles)
+
+        foreach (KeyValuePair<EnumNeighbour, Tuple<EnumTileDirection, Vector3Int>> neighbourTile in neighbourTiles)
         {
-            EnumNeighbour neighbour = neighbourTile.Key;
             EnumTileDirection neighbourTileDirection = neighbourTile.Value.Item1;
             Vector3Int neighbourPosition = neighbourTile.Value.Item2;
 
-            Debug.Log(neighbour.ToString() + " direction: " + neighbourTileDirection.ToString());
-            if(neighbourTileDirection != EnumTileDirection.None)
+            if (neighbourTileDirection != EnumTileDirection.None)
             {
-                if(neighbour == EnumNeighbour.Right)
-                {
-                    pathMap.SetTile(gridPosition, leftTile);
-                    pathMap.SetTile(neighbourPosition, leftTile);
-                    break;
-                }
-                else if(neighbour == EnumNeighbour.Left)
-                {
-                    pathMap.SetTile(gridPosition, rightTile);
-                    pathMap.SetTile(neighbourPosition, rightTile);
-                    break;
-                }
-                else if (neighbour == EnumNeighbour.Top)
-                {
-                    pathMap.SetTile(gridPosition, downTile);
-                    pathMap.SetTile(neighbourPosition, downTile);
-                    break;
-                }
-                else if (neighbour == EnumNeighbour.Bottom)
-                {
-                    pathMap.SetTile(gridPosition, upTile);
-                    pathMap.SetTile(neighbourPosition, upTile);
-                    break;
-                }
+                return neighbourPosition;
             }
         }
 
+        return null;
+    }
+    private void SetTiles(Vector3Int gridPosition)
+    {
+        if(gridPosition != previousTilePosition)
+        {
+            Tile setTile = null;
+
+            // set tile based on direction
+            if (gridPosition - Vector3Int.up == previousTilePosition) setTile = upTile;
+            else if (gridPosition - Vector3Int.down == previousTilePosition) setTile = downTile;
+            else if (gridPosition - Vector3Int.left == previousTilePosition) setTile = leftTile;
+            else setTile = rightTile;
+
+            // update tiles
+            Debug.Log("Setting tiles: " + gridPosition + " & " + previousTilePosition + " to " + setTile.name);
+            pathMap.SetTile(gridPosition, setTile);
+            pathMap.SetTile(previousTilePosition, setTile);
+
+            // update previous tile to current tile
+            previousTilePosition = gridPosition;
+            lastTile = setTile;
+        }
+        else
+        {
+            pathMap.SetTile(gridPosition, lastTile);
+        }
     }
 
+    private void ResetTilesToDefault()
+    {
+        // Reset start tile
+        if(startOriginalTile != null)
+        {
+            pathMap.SetTile(startTilePosition, startOriginalTile);
+            startOriginalTile = null;
+        }
+        foreach(Vector3Int cellPos in interactiveMap.cellBounds.allPositionsWithin)
+        {
+            pathMap.SetTile(cellPos, defaultTile);
+        }
+    }
 }
