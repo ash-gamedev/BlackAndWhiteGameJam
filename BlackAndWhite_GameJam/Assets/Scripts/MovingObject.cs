a81a362a14a59321beeb488c3548b904b0a53a84
ash-gamedev <adrouill1997@gmail.com>
2022-04-02 21:54:35 -0400

    Plate animation

--- BlackAndWhite_GameJam/Assets/Scripts/MovingObject.cs
+++ BlackAndWhite_GameJam/Assets/Scripts/MovingObject.cs
@@ -8,6 +8,7 @@ public class MovingObject : MonoBehaviour
     private TileManager tileConveyerManager;
     private float speed = 0;
     private Vector3 direction;
+    public bool canMove = false; 
 
     private void Awake()
     {
@@ -22,20 +23,23 @@ public class MovingObject : MonoBehaviour
 
     private void Update()
     {
-        Vector3 position = transform.position;
+        if (canMove)
+        {
+            Vector3 position = transform.position;
 
-        if (direction == Vector3.right)
-            position = new Vector3(position.x - 0.5f, position.y, position.z);
-        else if (direction == Vector3.left)
-            position = new Vector3(position.x + 0.5f, position.y, position.z);
-        else if (direction == Vector3.up)
-            position = new Vector3(position.x, position.y - 0.5f, position.z);
-        else if (direction == Vector3.down)
-            position = new Vector3(position.x, position.y + 0.5f, position.z);
+            if (direction == Vector3.right)
+                position = new Vector3(position.x - 0.5f, position.y, position.z);
+            else if (direction == Vector3.left)
+                position = new Vector3(position.x + 0.5f, position.y, position.z);
+            else if (direction == Vector3.up)
+                position = new Vector3(position.x, position.y - 0.5f, position.z);
+            else if (direction == Vector3.down)
+                position = new Vector3(position.x, position.y + 0.5f, position.z);
 
-        direction = tileConveyerManager.GetTileDirection(position);
+            direction = tileConveyerManager.GetTileDirection(position);
 
-        Vector3 moveVector = direction * Time.deltaTime * speed;
-        transform.position += moveVector;
+            Vector3 moveVector = direction * Time.deltaTime * speed;
+            transform.position += moveVector;
+        }
     }
 }
--- BlackAndWhite_GameJam/Assets/Scripts/Order.cs
+++ BlackAndWhite_GameJam/Assets/Scripts/Order.cs
@@ -6,9 +6,34 @@ public class Order : MonoBehaviour
     public EnumOrder order;
     [SerializeField] SpriteRenderer foodSpriteRenderer;
 
+    private Animator animator;
+    private MovingObject movingObject;
+
+    private float plateFallingAnimationLength = 2f;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        movingObject = GetComponent<MovingObject>();
+    }
+
     private void Start()
     {
+        // wait for animation for finish before beginning moving
+        GetPlateAnimationLength();
+        Invoke("OnPlateAnimationComplete", plateFallingAnimationLength);
+
         OrderManager orderManager = FindObjectOfType<OrderManager>();
         foodSpriteRenderer.sprite = orderManager.OrderSprites[order];
+    }
+
+    private void GetPlateAnimationLength()
+    {
+        plateFallingAnimationLength = (animator.runtimeAnimatorController.animationClips[0].length) * 0.8f;
+    }
+
+    private void OnPlateAnimationComplete()
+    {
+        movingObject.canMove = true;
     }
 }
